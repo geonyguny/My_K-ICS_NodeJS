4673e89cba2002298161a6f6a29a7de88a3bfef3
geony <purscean@gmail.com>
2023-02-17 23:48:18 +0900

    EJS 수정

--- controllers/kicsCtrl.js
+++ controllers/kicsCtrl.js
@@ -6,5 +6,5 @@ app.set('ejs',ejs.renderFile)
 
 exports.getKics = async function (req,res) {
     const[rows, fields] = await pool.query('SELECT * FROM LTerm_Summary');
-    res.render('form.ejs', {content: res})
+    res.render('form.ejs', {content: rows})
 };
