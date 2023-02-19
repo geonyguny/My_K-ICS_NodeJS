71c7d4493916692f73a771b8a48f6b0946d4ab04
geony <purscean@gmail.com>
2023-02-19 21:38:20 +0900

    업로드 수정

--- uploadExcel.js
+++ uploadExcel.js
@@ -1,41 +1,71 @@
 // const { pool } = require("./dbConfig");
 // const insertDao = require("./controllers/insertDao");
 // const express = require('express')
-// const readXlsxFile = require('read-excel-file/node')
 // const multer = require('multer')
 // const app = express()
 
-// exports.uploadExcel = async function () {
-//   try {
-//       const connection = await pool.getConnection(async (conn) => conn);
-//       console.log('up1')
-//       try{
-//         const storage = multer.diskStorage({
-//           destination: function (req, file, cb) {
-//             cb(null,  'uploads')
-//           },
-//           filename: function (req, file, cb) {
-//             cb(null, file.fieldname + '-' + Date.now())
-//           }
-//         });
-//         console.log('1')
-//         const uploadFile = multer({ storage: storage })
-//           app.get('/', (req, res) => {
-//             res.sendFile(__dirname + '/form.ejs')
-//           })
-//           console.log('2')
-//           app.post('/import-excel', uploadFile.single('import-excel'), (req, res) => {
-//             insertDao.insertKics(__dirname + '/uploads/' + req.file.filename)
-//           })
-//           connection.release();
-//           console.log('3')
-//       } catch (err) {
-//           console.error(" ##### Upload from Excel Query Error ##### ");
-//           connection.release();
-//           return false;
-//         }
-//     } catch (err) {
-//       console.error("##### DB Access Error #####");
-//       return false;
-//     }
-// };
+// // upload
+// pool.getConnection(function (err) {
+//   if (err) {
+//     return console.error('error: ' + err.message)
+//   }
+//   console.log('Database connected.')
+// })
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null,  'uploads')
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.fieldname + '-' + Date.now())
+//   }
+// });
+
+// const uploadFile = multer({ storage: storage })
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/views/form.ejs')
+// })
+// app.post('/import-excel', uploadFile.single('import-excel'), (req, res, next) => {
+//   res.status(200).send({
+//       message: "Ok",
+//       fileInfo: req.file
+//   })
+//   console.log('3')
+//   insertDao.insertKics(__dirname + '/uploads/' + req.file.filename)  
+// })
+
+
+// // exports.uploadExcel = async function () {
+// //   try {
+// //       const connection = await pool.getConnection(async (conn) => conn);
+      
+// //       try{
+// //         const storage = multer.diskStorage({
+// //           destination: function (req, file, cb) {
+// //             cb(null,  'uploads')
+// //           },
+// //           filename: function (req, file, cb) {
+// //             cb(null, file.fieldname + '-' + Date.now())
+// //           }
+// //         });
+// //         console.log('1')
+// //         const uploadFile = multer({ storage: storage })
+// //           app.get('/', (req, res) => {
+// //             res.sendFile(__dirname + '/views/form.ejs')
+// //           })
+// //           console.log('2')
+// //           app.post('/import-excel', uploadFile.single('import-excel'), (req, res, next) => {
+// //             insertDao.insertKics(__dirname + '/uploads/' + req.file.filename)
+// //           })
+// //           connection.release();
+// //           console.log('3')
+// //       } catch (err) {
+// //           console.error(" ##### Upload from Excel Query Error ##### ");
+// //           connection.release();
+// //           return false;
+// //         }
+// //     } catch (err) {
+// //       console.error("##### DB Access Error #####");
+// //       return false;
+// //     }
+// // };
