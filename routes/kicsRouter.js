e430184aa03bd963f516b8f60ded460dc21f9854
geony <purscean@gmail.com>
2023-02-27 18:23:54 +0900

    가용자본 쿼리 수정

--- routes/kicsRouter.js
+++ routes/kicsRouter.js
@@ -5,6 +5,7 @@ const deleteDao = require("../dao/deleteDao");
 const insertDao = require("../dao/insertDao");
 const MKqueryDao = require("../dao/MKqueryDao");
 const RTOqueryDao = require("../dao/RTOqueryDao");
+const AVCqueryDao = require("../dao/AVCqueryDao");
 const RQCqueryDao = require("../dao/RQCqueryDao");
 const CRqueryDao = require("../dao/CRqueryDao");
 const OPqueryDao = require("../dao/OPqueryDao");
@@ -16,7 +17,8 @@ const router = require("express").Router();
 router.route('/')
     .get(insertDao.getKics)
     .delete(deleteDao.deleteKics)    
-    .post(RQCqueryDao.queryKics)
+    .post(AVCqueryDao.queryKics)
+    // .post(RQCqueryDao.queryKics)
     // .post(RTOqueryDao.queryKics)
     //.post(insertDao.insertKics)   
     // .post(OPqueryDao.queryKics)
